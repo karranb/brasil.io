8ac2e109a637a2b1496189efcc6cfb8282c32f49
Karran Besen <karranrb@gmail.com>
2019-02-17 11:47:11 -0300

    revert: Fix: requested changes

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 from . import views, views_special
 
-sign_in_url = reverse_lazy('brasilio_auth:login')
+sign_up_url = reverse_lazy('brasilio_auth:sign_up')
 
 
 app_name = 'core'
@@ -22,7 +22,7 @@ urlpatterns = [
 
     # Dataset-specific pages (specials)
     path('especiais', views_special.index, name='specials'),
-    path('especiais/documento/<document>', login_required(views_special.document_detail, login_url=sign_in_url), name='special-document-detail'),
-    path('especiais/caminho', login_required(views_special.trace_path, login_url=sign_in_url), name='special-trace-path'),
-    path('especiais/grupos', login_required(views_special.company_groups, login_url=sign_in_url), name='special-company-groups'),
+    path('especiais/documento/<document>', login_required(views_special.document_detail, login_url=sign_up_url), name='special-document-detail'),
+    path('especiais/caminho', login_required(views_special.trace_path, login_url=sign_up_url), name='special-trace-path'),
+    path('especiais/grupos', login_required(views_special.company_groups, login_url=sign_up_url), name='special-company-groups'),
 ]
